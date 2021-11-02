43de8276d275aeaeb57e46c2c1c281ebf837dce9
AlexGG029 <alex.willy.gg@gmail.com>
2021-11-02 19:03:15 +0100

    Archivo Visual Studio - Actividad Git Clase 01

---
+++ Archivo Visual Studio 01/Archivo Visual Studio 01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Archivo_Visual_Studio_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Texto Modificable 
+            //Odio Ingeniería del Software
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
